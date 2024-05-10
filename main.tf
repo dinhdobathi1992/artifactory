dd0da88e35a280b255eaf4cf096a24492d00e4d6
Thi <67835317+dinhdobathi1992@users.noreply.github.com>
2024-05-10 20:24:13 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "artifactory_access_token" "new_token" {
 
   lifecycle {
     replace_triggered_by = [
-      time_rotating.time_rotating.rfc3339
+      time_rotating.thoundsandmin.rfc3339
     ]
   }
 }
